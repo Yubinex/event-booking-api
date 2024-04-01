5317642a9efbe7b2399604e970ae4a9c0ab3012f
Yubinex <janhauck99@gmail.com>
2024-04-01 20:09:55 +0200

    outsource routes

--- main.go
+++ main.go
@@ -1,66 +1,16 @@
 package main
 
 import (
-	"net/http"
-	"strconv"
-
 	"github.com/gin-gonic/gin"
 	"github.com/yubinex/event-booking-api/db"
-	"github.com/yubinex/event-booking-api/models"
+	"github.com/yubinex/event-booking-api/routes"
 )
 
 func main() {
 	db.InitDB()
 	server := gin.Default()
 
-	server.GET("/events", getEvents)
-	server.GET("/events/:id", getEvent)
-
-	server.POST("/events", createEvent)
+	routes.RegisterRoutes(server)
 
 	server.Run(":8080")
-}
-
-func getEvents(context *gin.Context) {
-	events, err := models.GetAllEvents()
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch events"})
-		return
-	}
-	context.JSON(http.StatusOK, events)
-}
-
-func getEvent(context *gin.Context) {
-	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse event id"})
-		return
-	}
-
-	event, err := models.GetEventById(eventId)
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch event"})
-	}
-
-	context.JSON(http.StatusOK, event)
-}
-
-func createEvent(context *gin.Context) {
-	var event models.Event
-	err := context.ShouldBindJSON(&event)
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request data"})
-		return
-	}
-
-	event.ID = 1
-	event.UserID = 1
-
-	err = event.Save()
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not create event"})
-		return
-	}
-
-	context.JSON(http.StatusCreated, gin.H{"message": "event created", "event": event})
 }
