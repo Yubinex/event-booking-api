90e242f21e2aab6cbcc11100ccdd26e15e2eb26d
Yubinex <janhauck99@gmail.com>
2024-04-03 05:11:44 +0200

    add login logic

--- models/user.go
+++ models/user.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"errors"
+
 	"github.com/yubinex/event-booking-api/db"
 	"github.com/yubinex/event-booking-api/utils"
 )
@@ -33,4 +35,22 @@ func (u User) Save() error {
 	userId, err := result.LastInsertId()
 	u.ID = userId
 	return err
+}
+
+func (u User) ValidateCredentials() error {
+	query := "SELECT password FROM users WHERE email = ?"
+	row := db.DB.QueryRow(query, u.Email)
+
+	var retrievedPassword string
+	err := row.Scan(&retrievedPassword)
+	if err != nil {
+		return errors.New("credentials invalid")
+	}
+
+	passwordIsValid := utils.CheckPasswordHash(u.Password, retrievedPassword)
+	if !passwordIsValid {
+		return errors.New("credentials invalid")
+	}
+
+	return nil
 }
