5a8f25f8cc28c944583679e087ca818771e500a4
Yubinex <janhauck99@gmail.com>
2024-04-03 03:34:13 +0200

    add user signup with hashed passwords

---
+++ routes/users.go
@@ -0,0 +1,26 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/yubinex/event-booking-api/models"
+)
+
+func signup(context *gin.Context) {
+	var user models.User
+
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request data"})
+		return
+	}
+
+	err = user.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not save user"})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "user created successfully"})
+}
