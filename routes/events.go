86b8c7a53056c63d1130cb24ee83fd2e511b45bf
Yubinex <janhauck99@gmail.com>
2024-04-06 23:45:12 +0200

    retrieving and storing user and event ids

--- routes/events.go
+++ routes/events.go
@@ -41,7 +41,7 @@ func createEvent(context *gin.Context) {
 		return
 	}
 
-	err := utils.VerifyToken(token)
+	userId, err := utils.VerifyToken(token)
 	if err != nil {
 		context.JSON(http.StatusUnauthorized, gin.H{"message": "not authorized"})
 		return
@@ -54,7 +54,7 @@ func createEvent(context *gin.Context) {
 		return
 	}
 
-	event.ID = 1
+	event.UserID = userId
 
 	err = event.Save()
 	if err != nil {
