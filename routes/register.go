ca5a9a457b3549c16d9638023c2b845d2682211d
Yubinex <janhauck99@gmail.com>
2024-04-07 00:45:08 +0200

    add registration functionality for events

---
+++ routes/register.go
@@ -0,0 +1,34 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/yubinex/event-booking-api/models"
+)
+
+func registerForEvent(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse event id"})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch event"})
+		return
+	}
+
+	err = event.Register(userId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not register user for event"})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "registered successfully"})
+}
+
+func cancelRegistration(context *gin.Context) {}
